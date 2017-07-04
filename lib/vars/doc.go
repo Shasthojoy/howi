1eff23ce82de40d8f11b743350bacd1d934533e8
Marko Kungla <marko.kungla@gmail.com>
2017-07-04 08:02:07 +0200

    Add package to have generic API to parse variables.
    Package vars provides the ability to parse variables from
    various input formats to common key value pair or sets to map[Var]Value.

---
+++ lib/vars/doc.go
@@ -0,0 +1,11 @@
+// Copyright 2017 Marko Kungla. All rights reserved.
+// Use of this source code is governed by a Apache License 2.0
+// license that can be found in the LICENSE file.
+
+/*
+Package vars provides the ability to parse variables from various input formats
+to common key value pair or sets to map[Var]Value.
+*/
+package vars
+
+// TODO(mkungla): Complete the docs
