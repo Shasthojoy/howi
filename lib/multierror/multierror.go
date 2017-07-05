89bcc1d725f660955c5fa1c54293a4e5733f9185
Marko Kungla <marko.kungla@gmail.com>
2017-07-05 13:15:54 +0200

    Add multierror package with tests

---
+++ lib/multierror/multierror.go
@@ -0,0 +1,58 @@
+package multierror
+
+import "fmt"
+
+// NewMultiError returns new multierror
+func NewMultiError() MultiError {
+	return MultiError{}
+}
+
+// MultiError is returned by batch operations when there are errors in
+// particular situations where returning single error is not possible
+// or type of error is not blocking further execution until MultiError
+// is evaluated.
+type MultiError struct {
+	Errors []error
+}
+
+// Error returns first occurred error string with additional suffix with
+// total count of errors.
+func (merr *MultiError) Error() (str string) {
+	l := len(merr.Errors)
+	if l > 0 {
+		str = merr.Errors[0].Error()
+	}
+	return fmt.Sprintf("%s (total errors: %d)", str, l)
+}
+
+// AppendError appends error to multierror if provided error is not nil
+func (merr *MultiError) AppendError(err error) error {
+	if err != nil {
+		n := len(merr.Errors)
+		if n == cap(merr.Errors) {
+			errs := make([]error, len(merr.Errors), 2*len(merr.Errors)+1)
+			copy(errs, merr.Errors)
+			merr.Errors = errs
+		}
+		merr.Errors = merr.Errors[0 : n+1]
+		merr.Errors[n] = err
+	}
+	return err
+}
+
+// AppendString creates error from provided string and appends it to multierror.
+// It also returns same created error if any case you would need it.
+// It returns nil if provided string is empty.
+func (merr *MultiError) AppendString(str string) error {
+	if str == "" {
+		return nil
+	}
+	err := fmt.Errorf("multierror: %s", str)
+	merr.AppendError(err)
+	return err
+}
+
+// Len returns total count of errors
+func (merr *MultiError) Len() int {
+	return len(merr.Errors)
+}
---
+++ lib/multierror/multierror_test.go
@@ -0,0 +1,34 @@
+package multierror
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewMultiError(t *testing.T) {
+	merr := NewMultiError()
+	tests := []struct {
+		errstr string
+	}{
+		{"new error"},
+		{"another error"},
+		{""},
+	}
+	for i := 0; i < len(tests); i++ {
+		err := merr.AppendString(tests[i].errstr)
+		actual := merr.AppendError(err)
+		if actual != err {
+			t.Errorf("multierror.AppendError() = %v, want %v", actual, err)
+		}
+
+		total := (i + 1) * 2
+		if tests[i].errstr != "" && merr.Len() != total {
+			t.Errorf("multierror.Len() = %v, want %v", merr.Len(), total)
+		}
+		expected := fmt.Sprintf("multierror: %s (total errors: %d)", tests[0].errstr, total)
+		if tests[i].errstr != "" && expected != merr.Error() {
+			t.Errorf("multierror.Error() = %q, want %q", merr.Error(), expected)
+		}
+	}
+
+}
