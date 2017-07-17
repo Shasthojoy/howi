ecfd8029ddcdf9d450c8433f6f8e92c2032b214d
Marko Kungla <marko.kungla@gmail.com>
2017-07-17 22:16:40 +0200

    Add cli worker and rename session to worker

--- pkg/hcli/command.go
+++ pkg/hcli/command.go
@@ -1,4 +1,30 @@
 package hcli
 
+import (
+	"github.com/howi-ce/howi/pkg/hcli/flags"
+	"github.com/howi-ce/howi/pkg/std/herrors"
+	"github.com/howi-ce/howi/pkg/vars"
+)
+
 // Command for application
-type Command struct{}
+type Command struct {
+	name           string
+	hidden         bool
+	category       string
+	usage          string
+	shortDesc      string
+	longDesc       string
+	errs           herrors.MultiError // internal errors
+	beforeFn       func(w *Worker)
+	doFn           func(w *Worker)
+	afterFailureFn func(w *Worker)
+	afterSuccessFn func(w *Worker)
+	afterAlwaysFn  func(w *Worker)
+	subCommands    map[string]Command      // subcommands
+	flags          map[int]flags.Interface // command flags
+	flagAliases    map[string]int          // command flag aliases
+	acceptArgs     int
+	args           []vars.Value
+	subCmd         *Command // if subcommand was called
+	parents        []string
+}
