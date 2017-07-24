dd01521e4e75991fbda4615cc8705ff779daea30
Marko Kungla <marko.kungla@gmail.com>
2017-07-25 00:49:53 +0200

    Add hos/hexec to use consistent Output handling from os/exec

---
+++ std/hos/hexec/output.go
@@ -0,0 +1,20 @@
+// Copyright 2005-2017 Marko Kungla. All rights reserved.
+// Use of this source code is governed by a Apache License 2.0
+// license that can be found in the LICENSE file.
+
+package hexec
+
+import "strings"
+
+// Output of git commands
+type Output []byte
+
+// Lines as string slice
+func (o Output) Lines() []string {
+	return strings.Split(string(o), "\n")
+}
+
+// String retruns string representation of output
+func (o Output) String() string {
+	return string(o)
+}
