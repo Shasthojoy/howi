69b4ee93e23dde3cc0eae648861a6581eb040f7f
Marko Kungla <marko.kungla@gmail.com>
2017-07-25 00:50:45 +0200

    Add HOWI Library documentation (replacement for old shared libs)

---
+++ lib/doc.go
@@ -0,0 +1,11 @@
+// Copyright 2005-2017 Marko Kungla. All rights reserved.
+// Use of this source code is governed by a Apache License 2.0
+// license that can be found in the LICENSE file.
+
+/*
+Package lib is collection of ccommonly shared libraries within addons and plugins.
+Faith of these libraries are unclear since their ancestor libraries were mostly shared objects.
+*/
+package lib
+
+// TODO(mkungla): Complete the docs
