86aa34ee2f057b293bd1cccea1ad6070a558e1bb
Marko Kungla <marko.kungla@gmail.com>
2017-08-06 10:27:07 +0200

    Rename hslices => slices

--- std/slices/int64.go
+++ std/slices/int64.go
@@ -2,25 +2,25 @@
 // Use of this source code is governed by a Apache License 2.0
 // license that can be found in the LICENSE file.
 
-package hslices
+package slices
 
 import "strconv"
 
-// MakeIntSlice returns new *IntSlice with optionally default values
-func MakeIntSlice(defaults ...int) *IntSlice {
-	return &IntSlice{raw: append([]int{}, defaults...)}
+// MakeInt64Slice returns new *Int64Slice with optionally default values
+func MakeInt64Slice(defaults ...int64) *Int64Slice {
+	return &Int64Slice{raw: append([]int64{}, defaults...)}
 }
 
-// IntSlice wraps an []int
-type IntSlice struct {
+// Int64Slice wraps a []int64
+type Int64Slice struct {
 	slice
-	raw []int
+	raw []int64
 }
 
 // Add parses the value into an integer and appends it to the list of values
-func (s *IntSlice) Add(value string) error {
+func (s *Int64Slice) Add(value string) error {
 	if !s.notEmpty {
-		s.raw = []int{}
+		s.raw = []int64{}
 		s.notEmpty = true
 	}
 
@@ -29,11 +29,11 @@ func (s *IntSlice) Add(value string) error {
 		return err
 	}
 
-	s.raw = append(s.raw, int(tmp))
+	s.raw = append(s.raw, tmp)
 	return nil
 }
 
-// Raw returns []int slice
-func (s *IntSlice) Raw() []int {
+// Raw returns []int64 slice
+func (s *Int64Slice) Raw() []int64 {
 	return s.raw
 }
