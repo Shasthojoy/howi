6c3b4cf9c45f17bbe75efe0165587283667e575d
Marko Kungla <marko.kungla@gmail.com>
2017-08-06 10:26:10 +0200

    Rename hstrings => strings

--- std/strings/doc.go
+++ std/strings/doc.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 /*
-Package hstrings implements simple functions which are not present
+Package strings implements simple functions which are not present
 in go standard library to manipulate strings.
 */
-package hstrings
+package strings
