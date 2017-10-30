5e97b0ff401715ce3812724f568494a84a4bc13a
Marko Kungla <marko.kungla@gmail.com>
2017-10-30 15:38:59 +0100

    Add howi-ce command

--- cmd/howi-ce/main.go
+++ cmd/howi-ce/main.go
@@ -1,12 +1,12 @@
-// Copyright 2005-2017 Marko Kungla. All rights reserved.
-// Use of this source code is governed by a Apache License 2.0
+// Copyright 2005-2017 Marko Kungla.
+// Use of this source code is governed by a The MIT License
 // license that can be found in the LICENSE file.
 
 package main
 
-import "github.com/howi-ce/howi/std/hlog"
+import "github.com/howi-ce/howi/std/log"
 
 func main() {
-	hlog.Colors()
-	hlog.Fatal("howi-ce is not yet available as command, use howi instead - see howi --help")
+	log.Colors()
+	log.Fatal("howi-ce is not yet available as command, use howi instead - see howi --help")
 }
