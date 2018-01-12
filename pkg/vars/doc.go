5757e774a0fd9024fcec175c9aac50b9b8294136
Marko Kungla <marko.kungla@gmail.com>
2018-01-12 18:29:02 +0100

    Add package vars related to #15

---
+++ pkg/vars/doc.go
@@ -0,0 +1,11 @@
+// Copyright 2012 Marko Kungla. All rights reserved.
+// Use of this source code is governed by a The Apache-style
+// license that can be found in the LICENSE file.
+
+/*
+Package vars provides the ability to parse variables from various input formats
+to common key value pair or sets to map[Var]Value.
+*/
+package vars
+
+// TODO(mkungla): Complete the docs
