e30118a7445a2b156931efe3de4d4ae7e38e9be3
Marko Kungla <marko.kungla@gmail.com>
2018-01-16 22:12:01 +0100

    Add slices pkg doc file

---
+++ pkg/slices/doc.go
@@ -0,0 +1,10 @@
+// Copyright 2012 Marko Kungla. All rights reserved.
+// Use of this source code is governed by a The Apache-style
+// license that can be found in the LICENSE file.
+
+// Package slices implements a doubly linked vars.Values
+//
+// it is inspired by container/list
+package slices
+
+// TODO(mkungla): Complete the docs
