696df72af1bf90ac31c873d830ae7cf1fb55abd4
Marko Kungla <marko.kungla@gmail.com>
2018-01-30 08:47:03 +0100

    Add tests for filesystem lib

---
+++ lib/filesystem/doc.go
@@ -0,0 +1,10 @@
+// Copyright 2016 Marko Kungla. All rights reserved.
+// Use of this source code is governed by a The Apache-style
+// license that can be found in the LICENSE file.
+
+/*
+Package filesystem provides filesystem abstraction for specific path like working directory.
+*/
+package filesystem
+
+// TODO(mkungla): Complete the docs
