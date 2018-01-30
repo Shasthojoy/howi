ede7f6d44e331bc4b2134ed475df1cc828f9cce0
Marko Kungla <marko.kungla@gmail.com>
2018-01-30 04:04:50 +0100

    prepare merge of howicli

--- lib/goprj/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2005-2017 Marko Kungla.
-// Use of this source code is governed by a The MIT License
-// license that can be found in the LICENSE file.
-
-/*
-Package goprj provides library to get information and work with your Go project.
-*/
-package goprj
--- std/strings/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2005-2017 Marko Kungla.
-// Use of this source code is governed by a The MIT License
-// license that can be found in the LICENSE file.
-
-/*
-Package strings implements simple functions which are not present
-in go standard library to manipulate strings.
-*/
-package strings
