8c04769662a261b2b2fcc42c66630cdb6e852391
Marko Kungla <marko.kungla@gmail.com>
2018-03-06 02:21:34 +0100

    Remove strings pkg ref #69

--- pkg/strings/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2017 Marko Kungla. All rights reserved.
-// Use of this source code is governed by a The Apache-style
-// license that can be found in the LICENSE file.
-
-/*
-Package strings implements simple functions which are not present
-in go standard library to manipulate strings.
-*/
-package strings
