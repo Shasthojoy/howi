f0d3e5c2e03ee6d2e43bfe34f0870a9f45169c4d
Marko Kungla <marko.kungla@gmail.com>
2018-03-06 03:43:31 +0100

    fix confirm ref 85db9fe9

--- cmd/howi/howi.go
+++ cmd/howi/howi.go
@@ -17,7 +17,7 @@ func main() {
 	appMeta.SetShortDesc("The extreme simplicity of HOWICLI makes the building of CLI applications in go super fun and easy. Includes collection of extended Go standard libraries, replacements, helpers and additional packages to transform HOWI API from it's other language bindings into Go.")
 	appMeta.SetKeywords("golang-tools", "go", "golang", "golang-library", "howi")
 	appMeta.SetCopyRight(2005, "Marko Kungla")
-	appMeta.SetVersion("5.0.0-alpha.a.2")
+	appMeta.SetVersion("5.0.0-alpha.a.3")
 	appMeta.SetURL("https://github.com/howi-ce/howi")
 	appMeta.AddContributor("Marko Kungla <marko.kungla@gmail.com>")
 	buildDate, err := time.Parse(time.RFC3339, "2018-03-06T03:06:34+02:00")
