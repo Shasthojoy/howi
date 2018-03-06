6cfa84013dfa90b9e11d0d8c5dd856266403c742
Marko Kungla <marko.kungla@gmail.com>
2018-03-06 02:20:11 +0100

    Add namespace package ref #68

---
+++ pkg/namespace/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2017 Marko Kungla. All rights reserved.
+// Use of this source code is governed by a The Apache-style
+// license that can be found in the LICENSE file.
+
+/*
+Package namespace implements simple functions which are not present
+in go standard library to manipulate strings for namespaces.
+*/
+package namespace
